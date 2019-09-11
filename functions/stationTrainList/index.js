891b0037c6fe658e2a26457a3f36cf070d243038
e <guansixu@gmail.com>
2019-09-11 15:45:22 +0800

    修改根据站点获取时刻表时的接口地址

--- functions/stationTrainList/index.js
+++ functions/stationTrainList/index.js
@@ -27,7 +27,7 @@ function formatTime(date) {
 exports.main = async (event, context) => {
   // const wxContext = cloud.getWXContext()
 
-  let url = "https://kyfw.12306.cn/otn/leftTicket/query"
+  let url = "https://kyfw.12306.cn/otn/leftTicket/queryA"
 
   let date = new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 1)
   let data = {
