0cd19b65585b343dee9c24894a298e42c050f4b9
e <guansixu@gmail.com>
2019-09-18 21:39:21 +0800

    去除强制更新

--- mini-program/app.js
+++ mini-program/app.js
@@ -2,29 +2,29 @@
 import { compareVersion } from './utils/util.js'
 App({
   onLaunch: function () {
-    const updateManager = wx.getUpdateManager()
+    // const updateManager = wx.getUpdateManager()
 
-    updateManager.onCheckForUpdate(function (res) {
-      // 请求完新版本信息的回调
-      console.log(res.hasUpdate)
-    })
+    // updateManager.onCheckForUpdate(function (res) {
+    //   // 请求完新版本信息的回调
+    //   console.log(res.hasUpdate)
+    // })
 
-    updateManager.onUpdateReady(function () {
-      wx.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，是否重启应用？',
-        success: function (res) {
-          if (res.confirm) {
-            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-            updateManager.applyUpdate()
-          }
-        }
-      })
-    })
+    // updateManager.onUpdateReady(function () {
+    //   wx.showModal({
+    //     title: '更新提示',
+    //     content: '新版本已经准备好，是否重启应用？',
+    //     success: function (res) {
+    //       if (res.confirm) {
+    //         // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+    //         updateManager.applyUpdate()
+    //       }
+    //     }
+    //   })
+    // })
 
-    updateManager.onUpdateFailed(function () {
-      // 新版本下载失败
-    })
+    // updateManager.onUpdateFailed(function () {
+    //   // 新版本下载失败
+    // })
 
     wx.cloud.init({
       env: 'phot-c9b2df'
